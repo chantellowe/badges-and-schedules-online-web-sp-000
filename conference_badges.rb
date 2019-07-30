1e9d98b7e0a8d731f5d008dd49bb58c8fb7f4726
Chantel Lowe <chantellowe17@gmail.com>
2019-07-30 15:18:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,35 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendee_badges = []
+  attendees.each do |name|
+    attendee_badges << badge_maker(name)
+  end
+  attendee_badges
+end
+
+def assign_rooms(attendees)
+  room_assignments = []
+  attendees.each.with_index(1) do |name, index|
+    room_assignments << "Hello, #{name}! You'll be assigned to room #{index}!"
+  end
+  room_assignments
+end
+
+def printer()
+  batch_badge_creator()
+  batch_badge_creator.each do |label|
+    puts label.chomp
+  end
+  
+  assign_rooms()
+  assign_rooms.each do |room|
+    puts room.chomp
+  end
+  
+  printer()
+end
